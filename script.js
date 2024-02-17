2a0bf912a492078dd034fb0990bde6a364912ad4
SarahB0307 <beldjoudi.sarah0307@gmail.com>
2024-02-17 14:45:44 +0100

    Version 1

---
+++ script.js
@@ -0,0 +1,6 @@
+function basculerMenu(){
+  const menu=document.querySelector(".liens-menu");
+  const icone=document.querySelector(".icone-hamburger");
+  menu.classList.toggle("open");
+  icone.classList.toggle("open");
+}
